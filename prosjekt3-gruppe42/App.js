ff27b0bdb6727a3d71f31f904d6fbe73498657d6
larsmhar <lars.martin.hardeland@gmail.com>
2018-09-26 11:13:33 +0200

    Wrote async storage and android date picker code. And it works #2

--- prosjekt3-gruppe42/App.js
+++ prosjekt3-gruppe42/App.js
@@ -1,11 +1,68 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, DatePickerAndroid, AsyncStorage } from 'react-native';
+
 
 export default class App extends React.Component {
+
+  constructor() {
+        super()
+        this.state = {
+                YearMonthDay: "Loading"
+        }
+      }
+
+  async test() {
+        try {
+      const {action, year, month, day} = await DatePickerAndroid.open({
+        // Use `new Date()` for current date.
+        // May 25 2020. Month 0 is January.
+        date: new Date()
+      });
+      if (action !== DatePickerAndroid.dismissedAction) {
+        // Selected year, month (0-11), day
+        this._storeData(year, month, day)
+        this._retrieveData()
+        console.warn(action, year, month, day)
+      }
+    } catch ({code, message}) {
+      console.warn('Cannot open date picker', message);
+    }
+  } 
+
+  _storeData = async (year, month, day) => {
+  try {
+    await AsyncStorage.setItem('YearMonthDay', year + "" + month + "" + day);
+  } catch (error) {
+    // Error saving data
+  }
+}
+
+  _retrieveData = async () => {
+  try {
+    const value = await AsyncStorage.getItem('YearMonthDay');
+    if (value !== null) {
+      // We have data!!
+      console.log(value);
+      this.setState({YearMonthDay: YearMonthDay})
+    }
+   } catch (error) {
+     // Error retrieving data
+     console.warn(error)
+   }
+}
+
+  componentDidMount() {
+      this._retrieveData()
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TouchableOpacity
+            onPress={()=>this.test()}
+        >
+        <Text>{ this.state.YearMonthDay}</Text>
+        </TouchableOpacity>
       </View>
     );
   }
